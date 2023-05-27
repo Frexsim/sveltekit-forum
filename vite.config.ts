3f2d7a95d8749cc904fdd05865c2feebafee75b1
iFrexsim <linusljunggren451@gmail.com>
2023-05-28 00:08:59 +0200

    Switched to BasicSsl

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import fs from "fs";
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
-		sveltekit()
+		sveltekit(),
+		basicSsl()
 	],
 	server: {
-		https: {
-			key: fs.readFileSync("ca.key"),
-			cert: fs.readFileSync("ca.crt"),
-		},
 		port: 8080,
 	},
 	preview: {
-		https: {
-			key: fs.readFileSync("ca.key"),
-			cert: fs.readFileSync("ca.crt"),
-		},
 		port: 8080,
 	},
 };
