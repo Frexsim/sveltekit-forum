9abdad86c47f4432e7e76f15879e867d272630c8
iFrexsim <linusljunggren451@gmail.com>
2023-05-29 20:57:42 +0200

    Updated about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,2 @@
-<h1>About</h1>
+<h1>About Cool Forum</h1>
 <p>This site is hosted on my personal computer so don't exepect it to be online all of the time. The site host automatically starts once i start my computer!</p>
